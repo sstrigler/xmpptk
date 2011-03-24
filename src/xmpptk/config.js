fd51528701876b5ed640fa04ef462183d3ac6acc
Stefan Strigler <stefan@strigler.de>
2011-03-24 18:01:15 +0100

    xmpptk.Config is a simple static object now

--- src/xmpptk/config.js
+++ src/xmpptk/config.js
@@ -1,18 +1,7 @@
 goog.provide('xmpptk.Config');
 
-/**
- * @constructor
- */
-xmpptk.Config = function(obj) {
-    for (var i in obj) {
-        if (obj.hasOwnProperty(i)) {
-            this[i] = obj[i];
-        }
-    }
-
-    // set some reasonable defaults
-    this.httpbase = obj.httpbase || '/http-bind/';
-    this.domain = obj.domain || 'localhost';
-    this.resource = obj.resource || 'helpim';
-
+xmpptk.Config = {
+    httpbase: '/http-bind/',
+    domain:   'localhost',
+    resource: 'xmpptk'
 };
