1fee02b6398c09fa6bcec11827145da9c9a1cac9
Stefan Strigler <stefan@strigler.de>
2011-03-24 20:02:23 +0100

    make room work

--- src/xmpptk/muc/room.js
+++ src/xmpptk/muc/room.js
@@ -2,8 +2,12 @@ goog.provide('xmpptk.muc.Room');
 goog.provide('xmpptk.muc.RoomJID');
 
 goog.require('goog.object');
+goog.require('goog.json');
+goog.require('goog.debug.Logger');
 
-goog.require('xmmptk.Model');
+goog.require('xmpptk.Model');
+goog.require('xmpptk.Client');
+goog.require('xmpptk.muc.Client');
 goog.require('xmpptk.muc.Roster');
 
 /** @typedef {{room: string, service: string, nick: string}} */
@@ -16,6 +20,7 @@ xmpptk.muc.RoomJid;
  * @param {xmpptk.Client} client 
  */
 xmpptk.muc.Room = function(room_jid, client) {
+    this._logger.info("creating room " + goog.json.serialize(room_jid));
     xmpptk.Model.call(this);
 
     // keep calm! it's better than you think, isn't it?
@@ -27,21 +32,24 @@ xmpptk.muc.Room = function(room_jid, client) {
     /** @type {xmpptk.muc.Roster} */
     this.roster = new xmpptk.muc.Roster();
 
-    /** @type {xmpptk.Client} */
-    this.client = client;
+    /** @private */
+    this._client = client;
 };
-goog.inherit(xmpptk.muc.Room, xmpptk.Model);
+goog.inherits(xmpptk.muc.Room, xmpptk.Model);
+
+xmpptk.muc.Room.prototype._logger = goog.debug.Logger.getLogger('xmpptk.muc.Room');
 
 xmpptk.muc.Room.prototype.join = function() {
+    this._logger.info("joining room "+this.jid);
     // setup handlers
 
     // send presence to rooms jid
-    this.client.sendPresence('available', undefined, this.jid);
+    this._client.sendPresence('available', undefined, this.jid);
 };
 
 xmpptk.muc.Room.prototype.part = function() {
     // disconnect handlers
 
     // send presence
-    this.client.sendPresence('unavailable', undefined, this.jid);
+    this._client.sendPresence('unavailable', undefined, this.jid);
 };
