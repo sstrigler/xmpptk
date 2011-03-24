510b59f4258cf7faedb4cad1eb949561940c35c5
Stefan Strigler <stefan@strigler.de>
2011-03-24 17:00:06 +0100

    use xmpptk.Client

--- src/xmpptk/muc/room.js
+++ src/xmpptk/muc/room.js
@@ -6,7 +6,6 @@ goog.require('goog.object');
 goog.require('xmmptk.Model');
 goog.require('xmpptk.muc.Roster');
 
-
 /** @typedef {{room: string, service: string, nick: string}} */
 xmpptk.muc.RoomJid;
 
@@ -14,16 +13,22 @@ xmpptk.muc.RoomJid;
  * @constructor
  * @inherits {xmpptk.Model}
  * @param {xmpptk.muc.RoomJID} room_jid Config to denote the rooms identity
+ * @param {xmpptk.Client} client 
  */
-xmpptk.muc.Room = function(room_jid) {
+xmpptk.muc.Room = function(room_jid, client) {
     xmpptk.Model.call(this);
 
     // keep calm! it's better than you think, isn't it?
     goog.object.extend(this, room_jid);
 
+    /** @type {string} */
     this.jid = this.room+'@'+this.service+'/'+this.nick;
 
+    /** @type {xmpptk.muc.Roster} */
     this.roster = new xmpptk.muc.Roster();
+
+    /** @type {xmpptk.Client} */
+    this.client = client;
 };
 goog.inherit(xmpptk.muc.Room, xmpptk.Model);
 
@@ -31,12 +36,12 @@ xmpptk.muc.Room.prototype.join = function() {
     // setup handlers
 
     // send presence to rooms jid
-    this.sendPresence('available', this.jid);
+    this.client.sendPresence('available', undefined, this.jid);
 };
 
 xmpptk.muc.Room.prototype.part = function() {
     // disconnect handlers
 
     // send presence
-    this.sendPresence('unavailable', this.jid);
+    this.client.sendPresence('unavailable', undefined, this.jid);
 };
