2b30e675e2e8c3096e0375e605f2a2da234c6c35
Stefan Strigler <stefan@strigler.de>
2011-03-25 14:28:54 +0100

    preliminary handler for groupchat messages

--- src/xmpptk/muc/client.js
+++ src/xmpptk/muc/client.js
@@ -13,6 +13,15 @@ xmpptk.muc.Client = function(client) {
 
     // will it blend?
     goog.object.extend(this, client);
+
+    // register handlers
+    this._con.registerHandler('message', '*', '*', 'groupchat', goog.bind(this._handleGroupchatMessage, this));
 };
 
 xmpptk.muc.Client.prototype._logger = goog.debug.Logger.getLogger('xmpptk.muc.Client');
+
+xmpptk.muc.Client.prototype._handleGroupchatMessage = function(oMsg) {
+    this._logger.info("handling muc message: "+oMsg.xml());
+
+    return true;
+};
