0823fedd1a38f1aee77891a0a404fbcb5f5ead87
Stefan Strigler <stefan.strigler@erlang-solutions.com>
2013-04-04 09:55:12 +0200

    revert ab5964988010b8875eff95ab8a374fd4ad42f03a

--- src/muc/client.js
+++
@@ -1,8 +0,0 @@
-goog.provide('muc.Client');
-
-goog.require('xmpptk.muc.Client');
-
-muc.Client = function() {
-    xmpptk.muc.Client.call(this);
-};
-goog.inherits(muc.Client, xmpptk.muc.Client);
--- src/muc/muc.js
+++
@@ -1,2 +0,0 @@
-goog.provide('muc');
-
